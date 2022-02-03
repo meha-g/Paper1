6a1575478315a6a7b36fb75aa0a2db6fab7c5aef
Meha Grewal <meha@Mehas-MacBook-Air-5.local>
2022-02-03 12:26:15 -0500

    data download

---
+++ starter_folder/scripts/01-data_download.R
@@ -0,0 +1,63 @@
+#### Preamble ####
+# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
+# Data: 3 January 2021
+# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+# - Change these to yours
+# Any other information needed?
+
+
+#### Workspace setup ####
+# Use R Projects, not setwd().
+library(haven)
+library(tidyverse)
+
+## Data Download
+#From Open Data Toronto https://open.toronto.ca/dataset/neighbourhood-crime-rates/
+
+library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("fc4d95a6-591f-411f-af17-327e6c5d03c7")
+package
+
+# get all resources for this package
+resources <- list_package_resources("fc4d95a6-591f-411f-af17-327e6c5d03c7")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+crime_rates <- filter(datastore_resources, row_number()==1) %>% get_resource()
+crime_rates
+
+##Save Data
+write_csv(crime_rates, "inputs/data/crime_rates.csv")
+#end
+
+
+
+# Read in the raw data. 
+raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+                     )
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(first_col, 
+         second_col)
+rm(raw_data)
+         
+
+#### What's next? ####
+
+
+
+         
